3950b955a4aa6f58c71f1ccca7ac4177816fdc2b
Samson Akinsanya <info@mobileparadigm.co.uk>
2022-02-18 11:50:52 +0100

    Adds pre-commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,35 @@ allprojects {
     }
 }
 
+val genericBaselineFile = file("$projectDir/detekt/baseline/detekt-baseline.xml")
+val moduleSpecificBaselineFile =
+    file("$projectDir/detekt/baseline/detekt-$subprojects}-baseline.xml")
+val baselineFile =
+    if (moduleSpecificBaselineFile.exists()) moduleSpecificBaselineFile else genericBaselineFile
+
+detekt {
+    baseline = baselineFile // a way of suppressing issues before introducing detekt
+    toolVersion = "1.16.0"
+    parallel = true
+    config = files("$projectDir/detekt/detekt-config.yml")
+    buildUponDefaultConfig = true
+    disableDefaultRuleSets = false
+    debug = false
+    ignoreFailures = false
+    reports {
+        xml {
+            enabled = false
+        }
+        html {
+            enabled = false
+        }
+        txt {
+            enabled = false
+        }
+    }
+}
+
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
