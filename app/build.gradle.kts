50107e49b8e5f781658624e41454154fb1c482b2
Samson Akinsanya <info@mobileparadigm.co.uk>
2022-02-18 12:52:42 +0100

    Adds key to release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         }
         getByName("release") {
             isMinifyEnabled = false
+            buildConfigField("String", "encryptionKey", key)
         }
     }
     packagingOptions {
